2ef8ea5b9898a24eb169d36fea82c946567cfa5e
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2025-09-19 23:32:39 +0900

    initial setting

---
+++ src/main/java/com/todaysound/todaysound_server/TodaysoundServerApplication.java
@@ -0,0 +1,13 @@
+package com.todaysound.todaysound_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysoundServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaysoundServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todaysound/todaysound_server/TodaysoundServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todaysound.todaysound_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysoundServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
