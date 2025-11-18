24d7aa978dd4d32af229c47d1dcd5a40299e30ae
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2025-11-19 01:24:19 +0900

    feat : #36 anonymous등록 수정

--- src/main/java/com/todaysound/todaysound_server/domain/user/service/UserCommandService.java
+++ src/main/java/com/todaysound/todaysound_server/domain/user/service/UserCommandService.java
@@ -24,7 +24,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserCommandService {
 
     private final UserRepository userRepository;
-    private final UserValidator userValidator;
     private final UserFactory userFactory;
     private final UserQueryService userQueryService;
 
@@ -34,46 +33,28 @@ public class UserCommandService {
         boolean secretExists =
                 userQueryService.existsBySecretFingerprint(userSecretRequestDto.deviceSecret());
 
-        User savedUser;
+        User user;
 
-        if (secretExists) {
-            log.info("User secret already exists");
-            User existingUser =
-                    userQueryService.findBySecretFingerprint(userSecretRequestDto.deviceSecret());
-
-            if (existingUser.hasFcmToken(userSecretRequestDto.fcmToken())) {
-                throw BaseException.type(AuthErrorCode.FCM_TOKEN_ALREADY_EXISTED);
-            }
-
-            // 중복 시크릿이 있고 fcmToken이 없다면 fcmToken만 추가
-            FCM_Token newFcmToken = FCM_Token.builder().fcmToken(userSecretRequestDto.fcmToken())
-                    .model(userSecretRequestDto.model()).user(existingUser).build();
-
-            log.info("New FCM_Token: {}", newFcmToken.getFcmToken());
-            existingUser.addFcmToken(newFcmToken);
-            savedUser = userRepository.save(existingUser);
-
-        } else {
-            log.info("User secret does not exist");
+        if (!secretExists) {
+            log.info("User secret does not exist, creating new user");
             log.info("fcmToken: {}", userSecretRequestDto.fcmToken());
-            // 중복 시크릿이 없다면 새로 유저 만들기
+
             User newUser = userFactory.createAnonymousUser(userSecretRequestDto);
 
-            // FCM Token 추가
             FCM_Token fcmToken = FCM_Token.builder().fcmToken(userSecretRequestDto.fcmToken())
                     .model(userSecretRequestDto.model()).user(newUser).build();
 
             newUser.addFcmToken(fcmToken);
 
-            // 데이터베이스에 저장
-            savedUser = userRepository.save(newUser);
+            user = userRepository.save(newUser);
+
+            user.clearPlainSecret();
+        } else {
+            log.info("User secret exists, returning existing user");
+            user = userQueryService.findBySecretFingerprint(userSecretRequestDto.deviceSecret());
         }
 
-        // 평문 시크릿 메모리에서 제거
-        savedUser.clearPlainSecret();
-
-        // DTO 변환 후 반환
-        return UserIdResponseDto.from(savedUser);
+        return UserIdResponseDto.from(user);
 
     }
 }
