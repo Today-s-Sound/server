c73ed78047d83dfee9b0be57e0b5b00e5b89f3d8
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2025-12-01 21:35:13 +0900

    feat : url도 추가로 return 되도록 수정

--- src/main/java/com/todaysound/todaysound_server/domain/alarm/dto/response/RecentAlarmResponse.java
+++ src/main/java/com/todaysound/todaysound_server/domain/alarm/dto/response/RecentAlarmResponse.java
@@ -4,12 +4,12 @@ import com.todaysound.todaysound_server.domain.summary.entity.Summary;
 import com.todaysound.todaysound_server.global.utils.TimeUtil;
 
 public record RecentAlarmResponse(Long subscriptionId, String alias, String summaryContent,
-        String timeAgo, boolean isUrgent) {
+        String url, String timeAgo, boolean isUrgent) {
 
     public static RecentAlarmResponse of(Summary summary) {
         return new RecentAlarmResponse(summary.getSubscription().getId(),
                 summary.getSubscription().getAlias(), summary.getContent(),
-                TimeUtil.toRelativeTime(summary.getUpdatedAt()),
+                summary.getSubscription().getUrl(), TimeUtil.toRelativeTime(summary.getUpdatedAt()),
                 summary.getSubscription().isUrgent());
     }
 
