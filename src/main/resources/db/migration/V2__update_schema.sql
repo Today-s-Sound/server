90f9f18c3bf47ff58161beb56374406e1c9c668d
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-01-12 00:50:14 +0900

    hotfix flyway

--- src/main/resources/db/migration/V2__update_schema.sql
+++ src/main/resources/db/migration/V2__update_schema.sql
@@ -1,7 +1,7 @@
 
 -- summaries: is_read 삭제, is_keyword_matched 추가
--- ALTER TABLE summaries DROP COLUMN is_read;
--- ALTER TABLE summaries ADD COLUMN is_keyword_matched BIT NOT NULL DEFAULT FALSE;
+ALTER TABLE summaries DROP COLUMN is_read;
+ALTER TABLE summaries ADD COLUMN is_keyword_matched BIT NOT NULL DEFAULT FALSE;
 
 -- subscriptions: is_urgent 삭제
 ALTER TABLE subscriptions DROP COLUMN is_urgent;
