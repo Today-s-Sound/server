3337034dfa65d5c9589ad47372d9a62a0df43d24
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2026-01-13 15:59:16 +0900

    feat: FeedControllerTest 추가

--- src/test/java/com/todaysound/todaysound_server/support/DocumentationTestSupport.java
+++ src/test/java/com/todaysound/todaysound_server/support/DocumentationTestSupport.java
@@ -2,12 +2,14 @@ package com.todaysound.todaysound_server.support;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.todaysound.todaysound_server.domain.feed.controller.FeedController;
+import com.todaysound.todaysound_server.domain.feed.service.FeedQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = {
@@ -26,5 +28,8 @@ public class DocumentationTestSupport {
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @MockitoBean
+    protected FeedQueryService feedQueryService;
 
 }
